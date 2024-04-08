bb3214135787d50c496a4eb8e8061ebec92808fd
slozero10 <slotzero10@gmail.com>
2024-04-08 18:26:30 +0800

    fix redstone

--- abi.js
+++ abi.js
@@ -1,49 +1,13 @@
 export const redstoneOracleAbi = [
-  {
-    inputs: [],
-    name: "CalldataMustHaveValidPayload",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "CalldataOverOrUnderFlow",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "CanNotPickMedianOfEmptyArray",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "DataPackageTimestampMustNotBeZero",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "DataPackageTimestampsMustBeEqual",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "EachSignerMustProvideTheSameValue",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "EmptyCalldataPointersArr",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "GetDataServiceIdNotImplemented",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "IncorrectUnsignedMetadataSize",
-    type: "error",
-  },
+  { inputs: [], name: "CalldataMustHaveValidPayload", type: "error" },
+  { inputs: [], name: "CalldataOverOrUnderFlow", type: "error" },
+  { inputs: [], name: "CanNotPickMedianOfEmptyArray", type: "error" },
+  { inputs: [], name: "DataPackageTimestampMustNotBeZero", type: "error" },
+  { inputs: [], name: "DataPackageTimestampsMustBeEqual", type: "error" },
+  { inputs: [], name: "EachSignerMustProvideTheSameValue", type: "error" },
+  { inputs: [], name: "EmptyCalldataPointersArr", type: "error" },
+  { inputs: [], name: "GetDataServiceIdNotImplemented", type: "error" },
+  { inputs: [], name: "IncorrectUnsignedMetadataSize", type: "error" },
   {
     inputs: [
       {
@@ -60,11 +24,7 @@ export const redstoneOracleAbi = [
     name: "InsufficientNumberOfUniqueSigners",
     type: "error",
   },
-  {
-    inputs: [],
-    name: "InvalidCalldataPointer",
-    type: "error",
-  },
+  { inputs: [], name: "InvalidCalldataPointer", type: "error" },
   {
     inputs: [],
     name: "RedstonePayloadMustHaveAtLeastOneDataPackage",
@@ -72,11 +32,7 @@ export const redstoneOracleAbi = [
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "receivedSigner",
-        type: "address",
-      },
+      { internalType: "address", name: "receivedSigner", type: "address" },
     ],
     name: "SignerNotAuthorised",
     type: "error",
@@ -88,11 +44,7 @@ export const redstoneOracleAbi = [
         name: "receivedTimestampSeconds",
         type: "uint256",
       },
-      {
-        internalType: "uint256",
-        name: "blockTimestamp",
-        type: "uint256",
-      },
+      { internalType: "uint256", name: "blockTimestamp", type: "uint256" },
     ],
     name: "TimestampFromTooLongFuture",
     type: "error",
@@ -104,31 +56,15 @@ export const redstoneOracleAbi = [
         name: "receivedTimestampSeconds",
         type: "uint256",
       },
-      {
-        internalType: "uint256",
-        name: "blockTimestamp",
-        type: "uint256",
-      },
+      { internalType: "uint256", name: "blockTimestamp", type: "uint256" },
     ],
     name: "TimestampIsTooOld",
     type: "error",
   },
   {
-    inputs: [
-      {
-        internalType: "uint256[]",
-        name: "values",
-        type: "uint256[]",
-      },
-    ],
+    inputs: [{ internalType: "uint256[]", name: "values", type: "uint256[]" }],
     name: "aggregateValues",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
     type: "function",
   },
@@ -136,100 +72,48 @@ export const redstoneOracleAbi = [
     inputs: [],
     name: "extractTimestampsAndAssertAllAreEqual",
     outputs: [
-      {
-        internalType: "uint256",
-        name: "extractedTimestamp",
-        type: "uint256",
-      },
+      { internalType: "uint256", name: "extractedTimestamp", type: "uint256" },
     ],
     stateMutability: "pure",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "signerAddress",
-        type: "address",
-      },
+      { internalType: "address", name: "signerAddress", type: "address" },
     ],
     name: "getAuthorisedSignerIndex",
-    outputs: [
-      {
-        internalType: "uint8",
-        name: "",
-        type: "uint8",
-      },
-    ],
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [],
     name: "getDataServiceId",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
+    outputs: [{ internalType: "string", name: "", type: "string" }],
     stateMutability: "view",
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "bytes32",
-        name: "dataFeedId",
-        type: "bytes32",
-      },
-    ],
+    inputs: [{ internalType: "bytes32", name: "dataFeedId", type: "bytes32" }],
     name: "getLatestPrice",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [],
     name: "getUniqueSignersThreshold",
-    outputs: [
-      {
-        internalType: "uint8",
-        name: "",
-        type: "uint8",
-      },
-    ],
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "bytes32[]",
-        name: "feedIds",
-        type: "bytes32[]",
-      },
-      {
-        internalType: "bytes[]",
-        name: "priceUpdateData",
-        type: "bytes[]",
-      },
+      { internalType: "bytes32[]", name: "feedIds", type: "bytes32[]" },
+      { internalType: "bytes[]", name: "priceUpdateData", type: "bytes[]" },
     ],
     name: "updatePrices",
-    outputs: [
-      {
-        internalType: "uint256[]",
-        name: "",
-        type: "uint256[]",
-      },
-    ],
+    outputs: [{ internalType: "uint256[]", name: "", type: "uint256[]" }],
     stateMutability: "payable",
     type: "function",
   },
--- test.js
+++ test.js
@@ -2,9 +2,9 @@ import { DataServiceWrapper } from "@redstone-finance/evm-connector";
 import { ethers } from "ethers";
 import { redstoneOracleAbi } from "./abi.js";
 
-const rpcUrl = "https://data-seed-prebsc-2-s1.bnbchain.org:8545";
-const oracleAddress = "0x7d9cEBe7BfE5093d6ed0301032f5afD168EF0ef2";
-const priceIds = ["ETH"];
+const rpcUrl = "https://bsc-dataseed2.bnbchain.org";
+const oracleAddress = "0x3903150043dc739926873F6F5f48c3061a55a4dD"; //"0x7d9cEBe7BfE5093d6ed0301032f5afD168EF0ef2"; //0x3903150043dc739926873F6F5f48c3061a55a4dD
+const priceIds = ["ETH", "USDT"];
 
 async function run() {
   const provider = new ethers.JsonRpcProvider(rpcUrl);
@@ -14,10 +14,25 @@ async function run() {
     provider
   );
   const redstonePayload = await new DataServiceWrapper({
+    dataServiceId: "redstone-primary-prod",
     dataFeeds: priceIds,
+    uniqueSignersCount: 3,
   }).getRedstonePayloadForManualUsage(oracleContract);
-
   console.log("redstonePayload: ", redstonePayload);
+
+  const redstonePayloadPrice = await new DataServiceWrapper({
+    dataFeeds: priceIds,
+    uniqueSignersCount: 1,
+    dataServiceId: "redstone-primary-prod",
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  }).getDataPackagesForPayload();
+  const ethPrice =
+    redstonePayloadPrice[0].dataPackage.dataPoints[0].numericDataPointArgs
+      .value;
+  const usdtPrice =
+    redstonePayloadPrice[1].dataPackage.dataPoints[0].numericDataPointArgs
+      .value;
+  console.log("redstonePayloadPrice: ", ethPrice, usdtPrice);
 }
 
 run();
